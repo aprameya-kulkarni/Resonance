fc590cbf52465cf747c1e26d520a4aa467b69783
Aprameya Kulkarni <apkdummy@gmail.com>
2026-09-24 12:07:39 +0530

    Project shelved
    Signed-off-by: Aprameya Kulkarni <apkdummy@gmail.com>

--- software/python/experiments/03_filter.py
+++ software/python/experiments/03_filter.py
@@ -3,6 +3,29 @@ import matplotlib.pyplot as plt
 from scipy import signal
 
 
+def fir_filter(samples, coefficients):
+    """
+    Causal FIR filter.
+
+    y[n] = sum(h[k] * x[n-k])
+    """
+    samples = np.asarray(samples)
+    coefficients = np.asarray(coefficients)
+
+    output = np.zeros_like(samples, dtype=float)
+
+    for n in range(len(samples)):
+        acc = 0.0
+
+        for k in range(len(coefficients)):
+            if n - k >= 0:
+                acc += coefficients[k] * samples[n - k]
+
+        output[n] = acc
+
+    return output
+
+
 fs = 10000
 duration = 0.1
 
@@ -39,6 +62,18 @@ filtered = signal.filtfilt(
 )
 
 
+coefficients = np.array([
+    0.25,
+    0.25,
+    0.25,
+    0.25
+])
+
+fir_output = fir_filter(
+    filtered,
+    coefficients
+)
+
 plt.figure()
 
 plt.plot(
@@ -51,6 +86,28 @@ plt.plot(
     t[:1000],
     filtered[:1000],
     label="After filter"
+)
+
+plt.xlabel("Time (s)")
+plt.ylabel("Amplitude")
+plt.legend()
+plt.grid()
+
+plt.show()
+
+
+plt.figure()
+
+plt.plot(
+    t[:1000],
+    filtered[:1000],
+    label="Before FIR"
+)
+
+plt.plot(
+    t[:1000],
+    fir_output[:1000],
+    label="After FIR"
 )
 
 plt.xlabel("Time (s)")
